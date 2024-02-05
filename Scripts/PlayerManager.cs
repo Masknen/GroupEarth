8718c935562ae797ceb9fce4649643c2129808f0
Max Stenlund <Prezzo55@gmail.com>
2024-02-05 13:43:10 +0100

    Update PlayerManager

--- Scripts/PlayerManager.cs
+++ Scripts/PlayerManager.cs
@@ -1,15 +1,31 @@
 using Godot;
 using System;
 
-public partial class PlayerManager : Node
+public partial class PlayerManager : Node3D
 {
+    public PlayerManager Instance { get; private set; }
+
+    public Godot.Collections.Array<Player> players {  get; private set; }
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        Input.JoyConnectionChanged += Input_JoyConnectionChanged;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
+
 	}
+
+    public void SpawnPlayers() {
+
+    }
+
+    private void Input_JoyConnectionChanged(long device, bool connected) {
+        
+    }
+
 }
