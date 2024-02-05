46dde2db3ab00c0f949d28c28c70fda42f308952
wholesomekarl <schelinkalle@gmail.com>
2024-02-05 17:52:31 +0100

    enemy1 script update

--- Scripts/enemy1.cs
+++ Scripts/enemy1.cs
@@ -1,6 +1,37 @@
 using Godot;
 using System;
+using System.Runtime;
+using System.Runtime.CompilerServices;
 
 public partial class enemy1 : CharacterBody3D
 {
+    [Export] public int moveSpeed = 10;
+  
+    [Signal] public delegate void HitEventHandler();
+  
+    private int hp = 1;
+
+    private Node3D target;
+
+    public override void _Ready()
+    {
+        target = GetNode<CharacterBody3D>("res://Scenes//player.tscn");
+    }
+
+    
+
+    public override void _Process(double delta)
+    {
+        
+    }
+    public void isHit()
+    {
+		EmitSignal(SignalName.Hit);
+        hp += -1;
+        if(hp == 0){
+            QueueFree();
+        }
+        
+	}
+
 }
