1651e618489d063708ec6f3f208b0bf5a5e36dd1
TheCoolStuntman <sebastian.ef.fransson@gmail.com>
2024-02-05 16:35:22 +0100

    Sebastian & Max turboskit

--- Scripts/PlayerCamera.cs
+++ Scripts/PlayerCamera.cs
@@ -3,26 +3,39 @@ using System;
 
 public partial class PlayerCamera : Camera3D
 {
-	private Godot.Collections.Array<Player> playerList;
+	private Godot.Collections.Array<Player> playerList = new Godot.Collections.Array<Player>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		playerList = PlayerManager.Instance().players;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (playerList.Count < 3){
-			var cameraPosX = (playerList[0].Position.X + playerList[1].Position.X) / 2;
-			var cameraPosZ = (playerList[0].Position.Z + playerList[1].Position.Z) / 2;
-			var cameraposY = playerList[0].Position.DistanceTo(playerList[1].Position);
+		playerList = PlayerManager.Instance().players;
+		// if (playerList.Count < 3){
+		//GD.Print(playerList);
+		// }
+		// else{
+		// 	GD.Print("Players not found by Camera Check PlayerCameras.cs");
+		// }
 
-			this.Position = new Vector3(cameraPosX, cameraposY, cameraPosZ);
-		}
-		else{
-			GD.Print("Players not found by Camera Check PlayerCameras.cs");
-		}
+		var cameraPosX = (playerList[0].Position.X + playerList[1].Position.X) / 2;
+		var cameraPosZ = (playerList[0].Position.Z + playerList[1].Position.Z) / 2;
+		var cameraposY = playerList[0].Position.DistanceTo(playerList[1].Position);
+
+		this.Position = new Vector3(cameraPosX, cameraposY, cameraPosZ);
+
+		// if (Input.IsActionJustPressed("spawnPlayers"))
+		// {
+		// 	PlayerManager.Instance().SpawnPlayers();
+
+		// }
+
+
+		GD.Print(cameraPosX, cameraposY, cameraPosZ);
+
 	}
 }
--- Scripts/PlayerManager.cs
+++ Scripts/PlayerManager.cs
@@ -12,11 +12,11 @@ public partial class PlayerManager : Node3D
         return _instance;
     }
 
-    public Godot.Collections.Array<Player> players {  get; private set; }
+    public Godot.Collections.Array<Player> players = new Godot.Collections.Array<Player>();
     private Godot.Collections.Array<int> playersToCreateID = new Godot.Collections.Array<int>();
     private PackedScene player;
 
-    private bool startJustPressed = true;
+    private bool[] startJustPressed = new bool[2];
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -30,9 +30,10 @@ public partial class PlayerManager : Node3D
     public override void _Process(double delta)
 	{
         var ConnectedJoyPadIDs = Input.GetConnectedJoypads();
+        int i = 0;
         foreach (var ID  in ConnectedJoyPadIDs) {
-            if (Input.IsJoyButtonPressed(ID, JoyButton.Start) && startJustPressed) {
-                startJustPressed = false;
+            if (Input.IsJoyButtonPressed(ID, JoyButton.Start) && startJustPressed[i]) {
+                startJustPressed[i] = false;
                 playersToCreateID.Add(ID);
                 GD.Print(playersToCreateID);
             }
@@ -40,12 +41,13 @@ public partial class PlayerManager : Node3D
                 playersToCreateID.Remove(ID);
                 GD.Print(playersToCreateID);
             }
-            if (!Input.IsJoyButtonPressed(ID, JoyButton.Start) && !startJustPressed) {
-                startJustPressed = true;
+            if (!Input.IsJoyButtonPressed(ID, JoyButton.Start) && !startJustPressed[i]) {
+                startJustPressed[i] = true;
             }
+            ++i;
         }
         if (playersToCreateID.Count >= 2) {
-            playersToCreateID.Remove(0);
+            playersToCreateID.RemoveAt(0);
         }
         if(Input.IsActionJustPressed("spawnPlayers")) {
             SpawnPlayers();
@@ -61,6 +63,7 @@ public partial class PlayerManager : Node3D
             AddChild(newPlayer);
 
             spawnOffsetX -= spawnOffsetX * 2;
+            players.Add(newPlayer as Player);
         }
     }
 
