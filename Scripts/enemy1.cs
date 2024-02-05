dcfd38da539e1a76f64c28ed4c8617374451a96f
wholesomekarl <schelinkalle@gmail.com>
2024-02-05 18:23:52 +0100

    ememy script update
    tracking update

--- Scripts/enemy1.cs
+++ Scripts/enemy1.cs
@@ -2,16 +2,21 @@ using Godot;
 using System;
 using System.Runtime;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class enemy1 : CharacterBody3D
 {
     [Export] public int moveSpeed = 10;
+    [Export] public float accelerationSpeed = 10.0f;
   
     [Signal] public delegate void HitEventHandler();
   
     private int hp = 1;
 
     private Node3D target;
+    private NavigationAgent3D nav;
+
+    private Vector3 direction;
 
     public override void _Ready()
     {
@@ -23,14 +28,25 @@ public partial class enemy1 : CharacterBody3D
     public override void _Process(double delta)
     {
         
+        //need to make some kind of AI tracking - NOT TESTED
+        Velocity = Vector3.Zero;
+        nav.TargetPosition = target.GlobalPosition;
+        direction = nav.GetNextPathPosition() - target.GlobalPosition;
+        direction = direction.Normalized();
+        Velocity = Velocity.Lerp(direction * moveSpeed, accelerationSpeed);
+        MoveAndSlide();  
+    }
+    public void die()
+    {
+        if(hp <= 0){
+            QueueFree();
+        }
     }
     public void isHit()
     {
 		EmitSignal(SignalName.Hit);
         hp += -1;
-        if(hp == 0){
-            QueueFree();
-        }
+        die();
         
 	}
 
