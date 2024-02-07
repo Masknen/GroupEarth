0109052eb2078076661e59d6dc0f2ec6f34bcf29
wholesomekarl <schelinkalle@gmail.com>
2024-02-07 15:54:50 +0100

    fix fierball

--- Scripts/FireBall.cs
+++ Scripts/FireBall.cs
@@ -2,17 +2,17 @@ using Godot;
 using System; 
 public partial class fireball : Area3D
 {
-	
-	public int speed = 10;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{	
-		
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		Position -= Transform.Basis.Z * (float)(speed * delta);
-	}
+    public int speed = 10;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        Position -= Transform.Basis.Z * (float)(speed * delta);
+    }
 }
