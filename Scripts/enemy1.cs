b02bfe3ead64cc21e1f78c9b6ad338e3481f0af5
wholesomekarl <schelinkalle@gmail.com>
2024-02-07 14:57:45 +0100

    enemy script update

--- Scripts/enemy1.cs
+++ Scripts/enemy1.cs
@@ -1,4 +1,5 @@
 using Godot;
+using GodotPlugins.Game;
 using System;
 using System.Runtime;
 using System.Runtime.CompilerServices;
@@ -12,15 +13,18 @@ public partial class enemy1 : CharacterBody3D
 	[Signal] public delegate void HitEventHandler();
   
 	private int hp = 1;
-
+	private PackedScene fireBall;
 	private Node3D target;
 	private CharacterBody3D player1;
 	private CharacterBody3D player2;
 	private NavigationAgent3D nav;
 	private Vector3 direction;
+	private float MaxTime = 2;
+	private float timeTick = 0;
 
     public override void _Ready()
     {
+		fireBall = GD.Load<PackedScene>("res://Scenes/FireBall.tscn");
         
     }
 
@@ -38,6 +42,11 @@ public partial class enemy1 : CharacterBody3D
 		Velocity = Position.DirectionTo(target.Position) * speed;
 		LookAtFromPosition(Position, target.GlobalPosition);
 		MoveAndSlide();
+		timeTick += (float)delta;
+		if (timeTick > MaxTime) {
+			timeTick -= MaxTime;
+			fireFireBall();
+		}
 		
 		/* WILL BE USED LATER TO READ THE MAP AND DODGE WALLS ECT...
 		//--get target position and move in that dircetion--
@@ -46,6 +55,14 @@ public partial class enemy1 : CharacterBody3D
 		direction = (nav.GetNextPathPosition() - target.GlobalPosition).Normalized();
 		Velocity = direction * moveSpeed;
 		MoveAndSlide(); */
+	}
+	public void fireFireBall(){
+		var new_fireBall = fireBall.Instantiate();
+		GD.Print("fireball!");
+		(new_fireBall as fireball).Position = Position;
+		//LookAt(target.GlobalPosition);
+		(new_fireBall as fireball).LookAtFromPosition(Position, target.GlobalPosition);
+		GetParent().AddChild(new_fireBall);
 	}
 
 	//make method to shoot a object twoards the closest player...
