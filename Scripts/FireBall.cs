bd4004362b8c47e854557115b7f99a6d28a755f6
Max Stenlund <Prezzo55@gmail.com>
2024-02-14 16:05:03 +0100

    Update FireBall.cs

--- Scripts/FireBall.cs
+++ Scripts/FireBall.cs
@@ -5,93 +5,88 @@ using System.Linq.Expressions;
 public partial class FireBall : Area3D, IDeflectable
 {
 
-	public int speed = 10;
-	public int baseSpeed = 10;
-	public int damage = 0;
-	
+    public int speed = 10;
+    public int baseSpeed = 10;
+    public int damage = 0;
+    
 
-	private float fireBallDuration = 20;
-	private float timeTick = 0;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		BodyEntered += FireBall_BodyEntered;
-		
-		//change color of the ball based on damage
-		
-		
-		
-	}
+    private float fireBallDuration = 20;
+    private float timeTick = 0;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        BodyEntered += FireBall_BodyEntered;
+        
+        //change color of the ball based on damage
+        
+        
+        
+    }
+    private void FireBall_BodyEntered(Node3D body) {
+        if (body as GridMap != null) QueueFree();
 
-	private void FireBall_BodyEntered(Node3D body) {
-		GD.Print(body as StaticBody3D + "||");
-		if (body as GridMap != null) QueueFree();
+        if (body as IDamagable != null) {
+            if ((body as IDamagable).Hit(10)) {
+                QueueFree();
+            }
+        }
+    }
 
-		GD.Print(body);
-		if (body as IDamagable != null) {
-			if ((body as IDamagable).Hit(damage)) {
-				QueueFree();
-			}
-		}
-		
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
-		timeTick += (float)delta;
-		if(timeTick > fireBallDuration){
-			QueueFree();
-		}
-		Position -= Transform.Basis.Z * (float)(speed * delta);
-		
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        
+        timeTick += (float)delta;
+        if(timeTick > fireBallDuration){
+            QueueFree();
+        }
+        Position -= Transform.Basis.Z * (float)(speed * delta);
+        
+    }
 
 
-	public void Deflect(float yRotation) {
-		damage += 1;
-		speed = baseSpeed + (3 * damage);
-		fireBallDuration = 20;
+    public void Deflect(float yRotation) {
+        damage += 1;
+        speed = baseSpeed + (3 * damage);
+        fireBallDuration = 20;
 
-		//change color
-		MeshInstance3D mesh = GetNode<MeshInstance3D>("MeshInstance3D");
-		StandardMaterial3D material = new StandardMaterial3D();  
-		material.AlbedoColor = new Color(1,0,01 * damage,0);
-		mesh.MaterialOverride = material;
-		//color changed
+        //change color
+        MeshInstance3D mesh = GetNode<MeshInstance3D>("MeshInstance3D");
+        StandardMaterial3D material = new StandardMaterial3D();  
+        material.AlbedoColor = new Color(1,0,01 * damage,0);
+        mesh.MaterialOverride = material;
+        //color changed
 
-		GD.Print(damage);
-		GD.Print("Hit");
-		Transform3D transform = new Transform3D(new Basis(Vector3.Up, yRotation), Position);
-		Transform = transform;
-	}
+        GD.Print(damage);
+        GD.Print("Hit");
+        Transform3D transform = new Transform3D(new Basis(Vector3.Up, yRotation), Position);
+        Transform = transform;
+    }
 
-	public void FriendDeflect(float yRotation) {
-		GD.Print("Hit");
-		damage += 1;
-		speed = baseSpeed + (3 * damage);
-		fireBallDuration = 20;
+    public void FriendDeflect(float yRotation) {
+        GD.Print("Hit");
+        damage += 1;
+        speed = baseSpeed + (3 * damage);
+        fireBallDuration = 20;
 
-		//change color
-		MeshInstance3D mesh = GetNode<MeshInstance3D>("MeshInstance3D");
-		StandardMaterial3D material = new StandardMaterial3D();
-		material.AlbedoColor = new Color(1, 0, 01 * damage, 0);
-		mesh.MaterialOverride = material;
-		//color changed
+        //change color
+        MeshInstance3D mesh = GetNode<MeshInstance3D>("MeshInstance3D");
+        StandardMaterial3D material = new StandardMaterial3D();
+        material.AlbedoColor = new Color(1, 0, 01 * damage, 0);
+        mesh.MaterialOverride = material;
+        //color changed
 
-		GD.Print(damage);
-		Transform3D transform = new Transform3D(new Basis(Vector3.Up, yRotation), Position);
-		Transform = transform;
-	}
+        GD.Print(damage);
+        Transform3D transform = new Transform3D(new Basis(Vector3.Up, yRotation), Position);
+        Transform = transform;
+    }
 
-	public void ArcDeflect(float yRotation) {
-		throw new NotImplementedException();
-	}
+    public void ArcDeflect(float yRotation) {
+        throw new NotImplementedException();
+    }
 
-	public void Hold() {
-		
-		speed = 0;
-	}
+    public void Hold() {
+        
+        speed = 0;
+    }
 }
