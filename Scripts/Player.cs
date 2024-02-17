091b866aaa48704144f486febb06b1f6a1cd9a6a
Max Stenlund <Prezzo55@gmail.com>
2024-02-17 16:58:23 +0100

    Update Player.cs
    Big

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,12 +1,6 @@
 using Godot;
-using Godot.Collections;
 using System;
 using System.Collections.Generic;
-using System.Security;
-//using System.Diagnostics;
-//using System.Security;
-
-//[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public partial class Player : CharacterBody3D, IDamagable {
 
 	public int ID = -1;
@@ -17,21 +11,24 @@ public partial class Player : CharacterBody3D, IDamagable {
 
     public Stat stats = new Stat();
 
-	private const float PARRY_DURATION = 0.2f;
-	private const float PARRY_COOLDOWN = 0.2f;
-	private const float DODGE_COOLDOWN = 0.3f;
+	private const float PARRY_DURATION        = 0.2f;
+	private const float PARRY_COOLDOWN        = 0.2f;
+	private const float DODGE_COOLDOWN        = 0.3f;
     private const float INVINCIBILTY_DURATION = 0.3f;
 
 	private float parryDurationTick = 0;
 	private float parryCooldownTick = 0;
 	private float dodgeCooldownTick = 0;
-    private float invincibiltyTick = 0;
+    private float invincibiltyTick  = 0;
 	private bool[] isParrying = {false, false, false};
+    private bool doDeflect = false;
 	private bool doDodge = false;
 
 
 	private AnimationPlayer animationPlayer;
 	private MeshInstance3D parryArea;
+    private float VisualCatchSize = 1.35f;
+    private StandardMaterial3D parryOverrideMaterial;
     private MeshInstance3D playerMarker;
 
     private PackedScene fireBall;
@@ -42,36 +39,41 @@ public partial class Player : CharacterBody3D, IDamagable {
 		animationPlayer = GetChild<AnimationPlayer>(3);
 		parryArea = GetChild<MeshInstance3D>(4);
 
-        GetChild<Area3D>(1).AreaEntered += Player_AreaEntered;
-		GetChild<Area3D>(1).AreaExited += Player_AreaExited;
+        GetChild<Area3D>(1).AreaEntered += ParryAreaEntered;
+		GetChild<Area3D>(1).AreaExited += ParryAreaExited;
 
-		parryArea.Visible = false;
+        parryOverrideMaterial = new StandardMaterial3D();
+        parryOverrideMaterial.Transparency = BaseMaterial3D.TransparencyEnum.Alpha;
+        parryOverrideMaterial.AlbedoColor = new Color(0, 0, 1, 0.1f);
+        parryArea.MaterialOverride = parryOverrideMaterial;
+        parryArea.Scale = Vector3.One * VisualCatchSize;
+        //parryArea.Visible = false;
 
         fireBall = GD.Load<PackedScene>("res://Scenes/fire_ball.tscn");
+        
         stats.AddStat(Stat.StatType.MaxHealth, 10).AddStat(Stat.StatType.MovementSpeed, 4).AddStat(Stat.StatType.DodgeStrength, 35).AddStat(Stat.StatType.RotationSpeed, 15);
         stats.AddStat(Stat.StatType.CurrentHealth, stats.GetStat(Stat.StatType.MaxHealth));
-	}
-
-	private void Player_AreaExited(Area3D area) {
-		currentTouching.Remove(area as IDeflectable);
-	}
-
-	private void Player_AreaEntered(Area3D area) {
-		if (area as IDeflectable != null) {
-			currentTouching.Add(area as IDeflectable);
-		}
 	}
 
 	public override void _Process(double delta) {
 		if (ID != -1) {
 			HandleInput();
+			//UpdateCooldownTicks(delta);
 
-			UpdateCooldownTicks(delta);
+
+            if (InputManager.Instance().IsJustPressedButton(ID, JoyButton.LeftShoulder)) {
+                for (int i = 0; i < 10; i++) {
+                    FireBall.Fire(Position, Transform.Rotated(Vector3.Up, (float)(Math.PI/5 * i)));
+                }
+            }
         }
 	}
 
+
     public override void _PhysicsProcess(double delta) {
 		if (ID != -1) {
+            UpdateDeflect(delta);
+
             if (!IsOnFloor()) {
                 Velocity -= new Vector3(0, 1, 0);
             }
@@ -117,45 +119,100 @@ public partial class Player : CharacterBody3D, IDamagable {
 	    MoveAndSlide();
 	}
 
-    private void HandleInput() {
-        NormalDeflect();
-        
-
-        if ((InputManager.Instance().IsJustPressedButton(ID, JoyButton.LeftShoulder) || isParrying[1]) && parryCooldownTick >= PARRY_COOLDOWN) {
-            isParrying[1] = true;
-            invincibiltyTick = INVINCIBILTY_DURATION;
-            parryCooldownTick = 0;
-            foreach (var defleactable in currentTouching) {
-                defleactable.FriendDeflect(-GlobalPosition.DirectionTo(otherPlayer.GlobalPosition).SignedAngleTo(Vector3.Forward, Transform.Basis.Y));
-            }
+    public void setID(int id) {
+        this.ID = id;
+        playerMarker = GetChild<MeshInstance3D>(5);
+        StandardMaterial3D material3D = new StandardMaterial3D();
+        switch (ID) {
+            case 0:
+                material3D.AlbedoColor = new Color(1.0f, 221.0f/255.0f, 0.0f, 1.0f);
+                playerMarker.MaterialOverride = material3D;
+                break; 
+            case 1:
+                material3D.AlbedoColor = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+                playerMarker.MaterialOverride = material3D;
+                break;
         }
+    }
+    bool IDamagable.Hit(int damage) {
+        if (invincibiltyTick < 0) {
+            stats.ModifyStat(Stat.StatType.CurrentHealth, -damage);
+            if (stats.GetStat(Stat.StatType.CurrentHealth) <= 0) {
+                Position = Vector3.Up;
+                stats.setStat(Stat.StatType.CurrentHealth, stats.GetStat(Stat.StatType.MaxHealth));
+            }
+            return true;
+        }
+        return false;
+    }
+
+	private void ParryAreaExited(Area3D area) {
+		currentTouching.Remove(area as IDeflectable);
+	}
+
+	private void ParryAreaEntered(Area3D area) {
+		if (area as IDeflectable != null) {
+            if (!isParrying[0]) {
+                currentTouching.Add(area as IDeflectable);
+            } else {
+                area.QueueFree();
+            }
+		}
+	}
+
+    private void HandleInput() {
+        if (Godot.Input.IsJoyButtonPressed(ID, JoyButton.RightShoulder)) {
+            isParrying[0] = true;
+
+            parryOverrideMaterial.AlbedoColor = new Color(0, 0, 1, 1);
+            parryArea.MaterialOverride = parryOverrideMaterial;
+            //parryArea.Visible = true;
+        }
+        if (InputManager.Instance().IsJustPressedButton(ID, JoyButton.RightShoulder)) {
+            parryArea.Scale = Vector3.One * VisualCatchSize;
+        }
+        if (InputManager.Instance().IsJustReleasedButton(ID, JoyButton.RightShoulder)) {
+            isParrying[0] = false;
+            doDeflect = true;
+            parryArea.Scale = Vector3.One * VisualCatchSize;
+
+            parryOverrideMaterial.AlbedoColor = new Color(0, 0, 1, 0.1f);
+            parryArea.MaterialOverride = parryOverrideMaterial;
+            //parryArea.Visible = false;
+        }
+
+        //NormalDeflect();
+        //if ((InputManager.Instance().IsJustPressedButton(ID, JoyButton.LeftShoulder) || isParrying[1]) && parryCooldownTick >= PARRY_COOLDOWN) {
+        //    isParrying[1] = true;
+        //    invincibiltyTick = INVINCIBILTY_DURATION;
+        //    parryCooldownTick = 0;
+        //    foreach (var defleactable in currentTouching) {
+        //        defleactable.FriendDeflect(-GlobalPosition.DirectionTo(otherPlayer.GlobalPosition).SignedAngleTo(Vector3.Forward, Transform.Basis.Y));
+        //    }
+        //}
 
         //Charge parry
-        if (Input.GetJoyAxis(ID, JoyAxis.TriggerRight) >= 0.15) {
-            parryArea.Visible = true;
-            float chargeSpeed = 0.05f;
-            if (InputManager.Instance().IsJustPressedAxis(ID, JoyAxis.TriggerRight)) {
-                parryArea.Scale = Vector3.Zero;
-            }
-            if (parryArea.Scale < Vector3.One) {
-                parryArea.Scale += new Vector3(chargeSpeed, chargeSpeed, chargeSpeed);
-            } else {
-                parryArea.Scale = Vector3.One;
-            }
-        }
+        //if (Input.GetJoyAxis(ID, JoyAxis.TriggerRight) >= 0.15) {
+        //    parryArea.Visible = true;
+        //    float chargeSpeed = 0.05f;
+        //    if (InputManager.Instance().IsJustPressedAxis(ID, JoyAxis.TriggerRight)) {
+        //        parryArea.Scale = Vector3.Zero;
+        //    }
+        //    if (parryArea.Scale < Vector3.One) {
+        //        parryArea.Scale += new Vector3(chargeSpeed, chargeSpeed, chargeSpeed);
+        //    } else {
+        //        parryArea.Scale = Vector3.One;
+        //    }
+        //}
 
-        if (InputManager.Instance().IsJustReleasedAxis(ID, JoyAxis.TriggerRight)) {
-            if (parryArea.Scale > new Vector3(0.95f, 0.95f, 0.95f)) {
-                invincibiltyTick = INVINCIBILTY_DURATION / 2.0f;
-                FireBall.Fire(Position, Transform);
-            }
-            parryArea.Scale = Vector3.One;
-            parryArea.Visible = false;
-        }
-        //
-
-
-
+        //if (InputManager.Instance().IsJustReleasedAxis(ID, JoyAxis.TriggerRight)) {
+        //    if (parryArea.Scale > new Vector3(0.95f, 0.95f, 0.95f)) {
+        //        invincibiltyTick = INVINCIBILTY_DURATION / 2.0f;
+        //        FireBall.Fire(Position, Transform);
+        //    }
+        //    parryArea.Scale = Vector3.One;
+        //    parryArea.Visible = false;
+        //}
         //if ((InputManager.Instance().IsJustPressedAxis(ID, JoyAxis.TriggerRight) || isParrying[2]) && parryCooldownTick >= PARRY_COOLDOWN) {
         //    GD.Print(ID + " | Arc Parry");
         //    invincibiltyTick = INVINCIBILTY_DURATION/2.0f;
@@ -173,21 +230,73 @@ public partial class Player : CharacterBody3D, IDamagable {
         //    //    defleactable.ArcDeflect(Transform.Basis.GetEuler().Y);
         //    //}
         //}
-
-
-
-
-
-
         if (InputManager.Instance().IsJustPressedAxis(ID, JoyAxis.TriggerLeft) && dodgeCooldownTick >= DODGE_COOLDOWN) {
             doDodge = true;
             invincibiltyTick = INVINCIBILTY_DURATION;
         }
     }
 
+
+    private void UpdateDeflect(double delta) {
+        if (isParrying[0]) {
+            foreach (var _item in currentTouching) {
+                var item = (_item as Node3D);
+                if (item == null) continue;
+
+                _item.Hold();
+                if (item.GlobalPosition.DistanceTo(this.GlobalPosition) > 2.5f) {
+                    Vector3 inwards = item.GlobalPosition.DirectionTo(this.GlobalPosition);
+                    item.GlobalPosition += inwards * (item.GlobalPosition.DistanceTo(this.GlobalPosition) * (float)(delta*3));
+                }
+            }
+
+            if (!parryArea.Scale.IsEqualApprox(Vector3.One)) {
+                parryArea.Scale = parryArea.Scale.Lerp(Vector3.One, (float)(delta * 15));
+            }
+        }
+        if (doDeflect) {
+            doDeflect = false;
+
+            var raycast = GetChild<RayCast3D>(6);
+            Node3D target = null;
+
+            float scanAngle = (float)(Math.PI);
+            raycast.Transform = new Transform3D(new Basis(Vector3.Up, -scanAngle), Vector3.Zero);
+            for (float i = -scanAngle; i <= scanAngle; i += (float)scanAngle/40) {
+                raycast.ForceRaycastUpdate();
+                var _object = raycast.GetCollider();
+                target = target == null ? _object as Node3D : target;
+
+                if ((_object as Node3D) != null) {
+                    //GD.Print(_object + " | " + i + " | target" + target + " | " + Position.DirectionTo(target.Position).AngleTo(-Transform.Basis.Z));
+                    var possibleTarget = _object as Node3D;
+                    if (Position.DirectionTo(target.Position).AngleTo(-Transform.Basis.Z) > Math.Abs(i)) {
+                        target = _object as Node3D;
+                    }
+                }
+                raycast.Transform = new Transform3D(new Basis(Vector3.Up, i), Vector3.Zero);
+            }
+            //GD.Print("Finish" + target);
+
+            foreach (var _item in currentTouching) {
+                var item = (_item as Node3D);
+                if (item == null) continue;
+
+                if (target != null) {
+                    var direction = item.GlobalPosition.DirectionTo(target.GlobalPosition);
+                    GD.Print(direction);
+                    float yRotation = -direction.SignedAngleTo(Vector3.Forward, Vector3.Up);
+                    _item.Deflect(yRotation);
+                } else {
+                    _item.Deflect(Transform.Basis.GetEuler().Y);
+                }
+            }
+        }
+    }
+
     private void NormalDeflect() {
         //Hold working(with centred circular deflect area)
-        if (Input.IsJoyButtonPressed(ID, JoyButton.RightShoulder) && parryCooldownTick >= PARRY_COOLDOWN && parryDurationTick < PARRY_DURATION) {
+        if (Godot.Input.IsJoyButtonPressed(ID, JoyButton.RightShoulder) && parryCooldownTick >= PARRY_COOLDOWN && parryDurationTick < PARRY_DURATION) {
             parryArea.Visible = true;
             isParrying[0] = true;
             parryDurationTick = 0;
@@ -237,8 +346,8 @@ public partial class Player : CharacterBody3D, IDamagable {
     }
 
 	private Vector2 GetInputVector(JoyAxis joyAxisX, JoyAxis joyAxisY, float deadZone) {
-        if (Input.GetJoyAxis(ID, joyAxisX) > deadZone || Input.GetJoyAxis(ID, joyAxisX) < -deadZone || Input.GetJoyAxis(ID, joyAxisY) > deadZone || Input.GetJoyAxis(ID, joyAxisY) < -deadZone) {
-			return new Vector2(Input.GetJoyAxis(ID, joyAxisX), Input.GetJoyAxis(ID, joyAxisY)).Normalized();
+        if (Godot.Input.GetJoyAxis(ID, joyAxisX) > deadZone || Godot.Input.GetJoyAxis(ID, joyAxisX) < -deadZone || Godot.Input.GetJoyAxis(ID, joyAxisY) > deadZone || Godot.Input.GetJoyAxis(ID, joyAxisY) < -deadZone) {
+			return new Vector2(Godot.Input.GetJoyAxis(ID, joyAxisX), Godot.Input.GetJoyAxis(ID, joyAxisY)).Normalized();
 		}
 		return Vector2.Zero;
 	}
@@ -267,35 +376,4 @@ public partial class Player : CharacterBody3D, IDamagable {
         transform.Basis = new Basis(quaternionTargetDirection);
         Transform = transform;
     }
-
-    public void setID(int id) {
-        this.ID = id;
-        playerMarker = GetChild<MeshInstance3D>(5);
-        StandardMaterial3D material3D = new StandardMaterial3D();
-        switch (ID) {
-            case 0:
-                material3D.AlbedoColor = new Color(1.0f, 221.0f/255.0f, 0.0f, 1.0f);
-                playerMarker.MaterialOverride = material3D;
-                break; 
-            case 1:
-                material3D.AlbedoColor = new Color(0.0f, 1.0f, 0.0f, 1.0f);
-                playerMarker.MaterialOverride = material3D;
-                break;
-        }
-    }
-    bool IDamagable.Hit(int damage) {
-        if (invincibiltyTick < 0) {
-            stats.ModifyStat(Stat.StatType.CurrentHealth, -damage);
-            if (stats.GetStat(Stat.StatType.CurrentHealth) <= 0) {
-                Position = Vector3.Up;
-                stats.setStat(Stat.StatType.CurrentHealth, stats.GetStat(Stat.StatType.MaxHealth));
-            }
-            return true;
-        }
-        return false;
-    }
-    //   private string GetDebuggerDisplay()
-    //{
-    //	return ToString();
-    //}
 }
