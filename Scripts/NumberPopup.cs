f949662a0f1117b46e7daf0430195285cdb12d4c
Max Stenlund <Prezzo55@gmail.com>
2024-02-20 15:53:23 +0100

    Update NumberPopup.cs

--- Scripts/NumberPopup.cs
+++ Scripts/NumberPopup.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class NumberPopup : Label3D
 {
-    private float lifeTime = 3;
+    private float lifeTime = 1.5f;
 
-    public static void Create(int number, Transform3D transform, Node parent) {
+    public static void Create(int number, Vector3 position, Node3D parent) {
         var numberPopup = GD.Load<PackedScene>("res://Scenes/GUI Scenes/number_popup.tscn");
         var instance = numberPopup.Instantiate();
         var new_numberPopup = (instance as Label3D);
         parent.GetParent().AddChild(instance);
-        new_numberPopup.Transform = transform;
-        new_numberPopup.Position += Vector3.Up*5;
-        new_numberPopup.Position += -Vector3.Forward;
+        //new_numberPopup.Transform = transform;
+        new_numberPopup.Rotation = MiddleNode.Instance.camera.Rotation;
+        new_numberPopup.Position = parent.Position + new_numberPopup.Transform.Basis.Z * 5;
 
         new_numberPopup.Text = "" + number;
     }
