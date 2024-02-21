61018e02e682e7299e671ebbcd4972e90783d1ba
Dawood <dawoodebrahimi6@gmail.com>
2024-02-21 13:50:44 +0100

    Create Menu.cs

---
+++ Scripts/GUI/Menu.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	private BaseButton startButton; 
+	private BaseButton quitButton; 
+
+	public EventHandler StartPressedEvent; 
+	public EventHandler QuitPressedEvent; 
+
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		startButton = 	GetChild(0).GetChild(0).GetChild<BaseButton>(0);
+		quitButtonButton = 	GetChild(0).GetChild(0).GetChild<BaseButton>(2);
+		startButton.Pressed += StartPressed; 
+		quitButton.Pressed += QuitPressed; 
+
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+	public void StartPressed()
+	{
+		StartPressedEvent?.Invoke(this,EventArgs.Empty); 
+
+	}
+	public void QuitPressed()
+	{
+		QuitPressedEvent?.Invoke(this,EventArgs.Empty); 
+
+	}
+	
+}
