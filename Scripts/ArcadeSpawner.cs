0ba2b5daafc1eb27fbda1e9b549ced500cd328a2
TheCoolStuntman <sebastian.ef.fransson@gmail.com>
2024-02-21 16:25:19 +0100

    Arcade spawner update
    Arcade spawner now places enemies around the players instead of in the middle

--- Scripts/ArcadeSpawner.cs
+++ Scripts/ArcadeSpawner.cs
@@ -18,7 +18,7 @@ public partial class ArcadeSpawner : Node3D {
     public override void _Ready() {
         enemy1 = GD.Load<PackedScene>("res://Scenes/enemy_1.tscn");
         enemy2 = GD.Load<PackedScene>("res://Scenes/enemy_2.tscn");
-        enemy3 = GD.Load<PackedScene>("res://Scenes/enemy_3.tscn");    
+        enemy3 = GD.Load<PackedScene>("res://Scenes/enemy_3.tscn");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,47 +29,49 @@ public partial class ArcadeSpawner : Node3D {
 
         //debug option F4----
         var enemies = GetTree().GetNodesInGroup("enemies");
-        if (Input.IsActionJustPressed("DespawnMobs") && PlayerManager.Instance().debugBoolean){
+        if (Input.IsActionJustPressed("DespawnMobs") && PlayerManager.Instance().debugBoolean) {
             mobsShouldSpawn = false;
-            foreach(var enemy in enemies){
+            foreach (var enemy in enemies) {
                 ((CharacterBody3D)enemy).QueueFree();
 
             }
         }
         //debug option F2
-        if(Input.IsActionJustPressed("SpawnMobs") && PlayerManager.Instance().debugBoolean){
+        if (Input.IsActionJustPressed("SpawnMobs") && PlayerManager.Instance().debugBoolean) {
             mobsShouldSpawn = true;
         }
-        
-        if(mobsShouldSpawn){
+
+        if (mobsShouldSpawn) {
             if (spawnTimer >= 4) {
 
-            var spawnedEnemy = enemy1.Instantiate(); //It needs a default
-            currentTokens += 1;
+                var spawnedEnemy = enemy1.Instantiate(); //It needs a default
+                currentTokens += 1;
 
-            if (chosenEnemy == 1) {
-                spawnedEnemy = enemy2.Instantiate();
-                currentTokens += 2;
+                if (chosenEnemy == 1) {
+                    spawnedEnemy = enemy2.Instantiate();
+                    currentTokens += 2;
+                }
+                if (chosenEnemy == 2) {
+                    spawnedEnemy = enemy3.Instantiate();
+                    currentTokens += 4;
+                }
+
+                GetTree().Root.AddChild(spawnedEnemy);
+                float dirAngle = GD.Randf() * (2 * MathF.PI);
+                Transform3D lookTransfrom = new Transform3D(new Basis(Transform.Basis.Y, dirAngle), new Vector3(0, 2, 0));
+
+                (spawnedEnemy as CharacterBody3D).Transform = GetParentNode3D().Transform;
+                (spawnedEnemy as CharacterBody3D).Position += lookTransfrom.Basis.Y;
+                (spawnedEnemy as CharacterBody3D).Position += -lookTransfrom.Basis.Z * 17;
+
+
+                if (currentTokens >= maxTokens) {
+                    spawnTimer = 0;
+                    currentTokens = 0;
+                }
             }
-            if (chosenEnemy == 2) {
-                spawnedEnemy = enemy3.Instantiate();
-                currentTokens += 4;
-            }
 
-            GetTree().Root.AddChild(spawnedEnemy);
-            float dirAngle = GD.Randf() * (2 * MathF.PI);
-            Transform3D lookTransfrom = new Transform3D(new Basis(Transform.Basis.Y, dirAngle), new Vector3( 0, 2, 0 ));
-
-            (spawnedEnemy as CharacterBody3D).Position += lookTransfrom.Basis.Y;
-            (spawnedEnemy as CharacterBody3D).Position += -lookTransfrom.Basis.Z * 17;
-
-            if (currentTokens >= maxTokens) {
-                spawnTimer = 0;
-                currentTokens = 0; 
-            }
         }
-        
     }
-    }
-    
+
 }
