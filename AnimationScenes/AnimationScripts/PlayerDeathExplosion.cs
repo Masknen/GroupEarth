a868ab07261fb20768badb308b880a7d81beeebd
wholesomekarl <schelinkalle@gmail.com>
2024-02-26 18:16:58 +0100

    player death and ress effects made

---
+++ AnimationScenes/AnimationScripts/PlayerDeathExplosion.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class PlayerDeathExplosion : Node3D
+{
+    private float MaxTime = 0.5f;
+	private float timeTick = 0;
+    
+    public override void _Ready(){
+        
+
+    }
+    public override void _Process(double delta)
+    {
+        
+        timeTick += (float)delta;
+				if (timeTick > MaxTime) {
+					QueueFree();	
+				}
+                
+        
+    }
+}
