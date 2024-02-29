3198ba02bf8412f2630747315f1473ade34b0ec8
Max Stenlund <Prezzo55@gmail.com>
2024-02-29 11:40:45 +0100

    Made Breakable wall, only when strong enough i think

---
+++ Scripts/TutorialWall.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class TutorialWall : StaticBody3D, IDamagable {
+    public bool Hit(int damage) {
+        if(damage >= 4) {
+            QueueFree(); 
+            return true;
+        }
+        return false;
+    }
+}
