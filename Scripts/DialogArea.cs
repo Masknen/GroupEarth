1f68069ac4acc1f72892c0d76baa175f96bba26a
wholesomekarl <schelinkalle@gmail.com>
2024-03-02 14:18:19 +0100

    subtitles timer on first wall

--- Scripts/DialogArea.cs
+++ Scripts/DialogArea.cs
@@ -5,14 +5,43 @@ public partial class DialogArea : Area3D
 {
     
     private bool runOnce = true;
+
+    private Label dialog;
+    private Label hint;
+    private Timer timer;
+    private Timer hintTimer;
+
     public override void _Ready() {
         BodyEntered += SpawnerActivater_BodyEntered;
+        dialog = GetNode<Label>("Label1");
+        timer = GetNode<Timer>("DialogTimer");
+        hintTimer = GetNode<Timer>("HintTimer");
+        hint = GetNode<Label>("Label2");
+        timer.Timeout += dialogVisibleSwitch;
+        hintTimer.Timeout += hintVisibleSwitch;
+    }
+
+    private void dialogVisibleSwitch(){
+        if(dialog.Visible){
+            dialog.Visible = false;
+        }
+        
+    }
+    private void hintVisibleSwitch(){
+        if(hint.Visible){
+            hint.Visible = false;
+        }
+        
     }
 
     private void SpawnerActivater_BodyEntered(Node3D body) {
         GD.Print(body);
         if ((body as Player) != null) {
             if(runOnce){
+                dialog.Visible = true;
+                hint.Visible = true;
+                hintTimer.Start();
+                timer.Start();
                 SoundManager.Instance.PlayDialogOne();
                 runOnce = false;
             }
